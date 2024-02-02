274e322e8ba5a12767493c0d718d0faeb661e207
Elias-s231135 <s231135@students.aie.edu.au>
2024-02-02 16:46:00 +1030

    Circle class

---
+++ Circle class/Circle class/Circle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Circle
+{
+public:
+	void SetRadius(float f);
+	float GetRadius();
+	float GetArea();
+	float GetCircumference();
+
+private:
+	float m_radius;
+};
---
+++ Circle class/Circle class/Exercise.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Circle.h"
+#include <string>
+extern const float PI = 3.142f;
+
+int main()
+{
+	Circle circle1 = ();
+
+	std::cout << circle1.GetRadius() << std::endl;
+
+	circle1.SetRadius(3.141f);
+
+	std::cout << circle.GetRadius() << std::endl;
+
+	std::cout << "Area = " << circle1.GetArea() << std::endl;
+
+	std::cout << "Circumference = " << circle1.GetCircumference() << std::endl;
+
+	std::string string1 = "Hello I am a string";
+
+	std::cout << string1 << std::endl;
+}
