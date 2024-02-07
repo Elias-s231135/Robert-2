a73bd842f60cb8dc5511abca1fc554823db7c8c2
Elias-s231135 <s231135@students.aie.edu.au>
2024-02-07 16:10:11 +1030

    Constructor and Destructor Part 2

--- Constructor and Destructor/Constructor and Destructor/Player.h
+++ Constructor and Destructor/Constructor and Destructor/Player.h
@@ -1,18 +1,40 @@
 #pragma once
 
+//class Player
+//{
+//public:
+//	Player();
+//	Player(const char* name);
+//	Player(int a_max_ammo, int a_max_health);
+//	Player(float x, float y);
+//	Player(Player& a_player);
+//
+//	float X, Y;
+//	int ammo;
+//	int max_ammo;
+//	int health;
+//	int max_health;
+//	char name[64];
+//};
+
 class Player
 {
 public:
-	Player();
-	Player(const char* name);
-	Player(int a_max_ammo, int a_max_health);
-	Player(float x, float y);
-	Player(Player& a_player);
+	struct Bullet { float x, y; };
 
-	float X, Y;
-	int ammo;
-	int max_ammo;
+	Player(int max_ammo)
+	{
+		bullets = new Bullet[max_ammo];
+		ammo = 0;
+		health = 0;
+		max_health = 100;
+	}
+
+	~Player(); // implement this
+
 	int health;
 	int max_health;
-	char name[64];
+	int ammo;
+	int max_ammo;
+	Bullet* bullets;
 };
